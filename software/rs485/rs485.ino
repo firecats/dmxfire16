c8b08918a822f0fa969f6e51c59d814107830bfb
Jody McIntyre <scjody@modernduck.com>
2017-10-06 16:48:43 -0400

    Improve debugging & make it optional

--- software/rs485/rs485.ino
+++ software/rs485/rs485.ino
@@ -10,6 +10,8 @@
 #define STOP 0x7e
 #define ESCAPE 0x20
 
+#define DEBUG 1
+
 void setup() {
   // put your setup code here, to run once:
   Tlc.init();
@@ -32,41 +34,38 @@ int n = 0;
 int offset = BOARD * 4;
 
 void loop() {
-  
   if(Serial.available() > 0) {
-  /*
-    int n_read = Serial.readBytes(buf, 1024);
-    for (int i = 0; i < n_read; i++) {
-      // copy packet to buffer
-      
-      
-    }
-   */ 
-    
     
     int in = Serial.read();
-    //Serial.print("read 0x");
-    //Serial.println(in, HEX);
-
+    if (DEBUG) {
+      Serial.print("read 0x");
+      Serial.println(in, HEX);
+    }
     
     if (in == START) {
       reading = true;
       n = 0;
-      Serial.println("start");
+      if (DEBUG) {
+        Serial.println("start");
+      }
     } else if (in == STOP) {
       reading = false;
       to_fire = true;
-      Serial.println("stop");
+      if (DEBUG) {
+        Serial.println("stop");
+      }
     } else if (reading) {
       if (in == ESCAPE) {
         in = Serial.read() ^ ESCAPE;
-        Serial.println("escape");
+        if (DEBUG) {
+          Serial.println("escape");
+        }
       }   
       buf[n++] = in;
     }
   }
   
-  if (!reading) {
+  if (DEBUG && !reading) {
     for (int i = 0; i < 33; i++) {
       Serial.println(buf[i], HEX);
     }
@@ -84,17 +83,22 @@ void loop() {
     if (csum == buf[32] && to_fire) {
       // send 4 bytes starting at "offset"
       Tlc.clear();
-      
-      Serial.println("checksum OK, firing!");
+
+      if (DEBUG) {
+        Serial.println("checksum OK, firing!");
+      }
       for (char s = 0; s < 32; s++) {
         char by = s / 8 + offset;
         char bi = s % 8;
         char on = (buf[by] >> bi) & 0x01;
         int val = on ? 4095 : 0;
         Tlc.set(s, val);
-        Serial.print(s);
-        Serial.print(": ");
-        Serial.println(val);
+        if (DEBUG) {
+          int foo = s;
+          Serial.print(foo);
+          Serial.print(": ");
+          Serial.println(val);
+        }
       }
       Tlc.update();
       to_fire = false;
